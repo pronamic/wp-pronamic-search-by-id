d0bd2e2b516605f76fe06b393474094bcbd96bab
Remco Tolsma <info@remcotolsma.nl>
2013-11-28 09:12:54 +0100

    Init commit.

---
+++ pronamic-search-by-id.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Pronamic Search by ID
+Plugin URI: http://wp.pronamic.eu/plugins/pronamic-search-by-id/
+Description: Enables the user to search by post ID using the built-in search. Works for all kinds of posts (posts, pages, custom post types).
+
+Version: 1.0.0
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://www.pronamic.eu/
+
+Text Domain: pronamic_search_by_id
+Domain Path: /languages/
+
+License: GPL
+
+GitHub URI: https://github.com/pronamic/wp-pronamic-search-by-id
+*/
+
+function pronamic_search_by_id_where( $where ) {
+	if ( is_search() ) {
+		$s = filter_input( INPUT_GET, 's', FILTER_SANITIZE_STRING );
+
+		if ( ! empty( $s ) ) {
+			global $wpdb;
+			
+			if ( is_numeric( $s ) ) {
+				$where .= ' OR ' . $wpdb->posts . '.ID = ' . $s;
+			} elseif( preg_match( "/^(\d+)(,\s*\d+)*\$/", $s ) ) { // string of post IDs
+				$where .= ' OR ' . $wpdb->posts . '.ID in (' . $s . ')';
+			}
+		}
+	}
+
+	return $where;
+}
+
+add_filter( 'posts_where', 'pronamic_search_by_id_where' );
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Pronamic Search by ID ===
+Contributors: pronamic, remcotolsma
+Donate link: http://pronamic.eu/donate/?for=wp-plugin-pronamic-search-by-id&source=wp-plugin-readme-txt
+Tags: search, id
+Requires at least: 3.0.1
+Tested up to: 3.7.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enables the user to search by post ID using the built-in search. Works for all kinds of posts (posts, pages, custom post types).
+
+
+== Changelog ==
+
+= 1.0.0 =
+*	Initial release.
+
